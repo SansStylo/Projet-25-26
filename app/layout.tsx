d9d58ebe671b571576b19b4d911b4f5cb0ab86db
Audrey-Vasseur <audrey.vasseur@student.junia.com>
2026-05-27 15:56:28 +0200

    Ajout visuel de la page d'accueil enseignant

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Junia'lytics - Connexion",
+  title: "Junia'lytics",
   description: "Plateforme de suivi pédagogique",
 };
 
