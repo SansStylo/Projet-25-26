7457136de981bd682d6e0140f2ab730f5cdd1aa7
noeprt21 <portenartn@gmail.com>
2026-06-10 00:04:49 +0200

    fix sidebar rattrapages

--- app/components/responsable/ResponsableLayout.tsx
+++ app/components/responsable/ResponsableLayout.tsx
@@ -96,6 +96,7 @@ export default function ResponsableClientLayout({ children, user }: { children: 
             { name: 'Matières', href: '/responsable/matieres', icon: <><rect width="8" height="18" x="3" y="3" rx="1"></rect><path d="M7 3v18"></path><path d="M20.4 18.9c.2.5-.1 1.1-.6 1.3l-1.9.7c-.5.2-1.1-.1-1.3-.6L11.1 5.1c-.2-.5.1-1.1.6-1.3l1.9-.7c.5-.2 1.1.1 1.3.6Z"></path></> },
             { name: 'Étudiants', href: '/responsable/etudiants', icon: <><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></> },
             { name: 'Saisie des notes', href: '/responsable/notes', icon : <><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></>},
+            { name: 'Rattrapages', href: '/responsable/rattrapages', icon: <><path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8"></path><path d="M3 3v5h5"></path><path d="M12 7v5l4 2"></path></> },
             { name: 'Rapports', href: '/responsable/rapports', icon: <><line x1="18" y1="20" x2="18" y2="10"></line><line x1="12" y1="20" x2="12" y2="4"></line><line x1="6" y1="20" x2="6" y2="14"></line></> }
           ].map((item, index) => {
             const isActive = item.href === '/responsable' ? pathname === item.href : pathname.startsWith(item.href);
